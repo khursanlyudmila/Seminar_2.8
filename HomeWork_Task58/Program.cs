bd016a78d60b80b3ba405e13e4394ef7ddbbb118
Khursan Lyudmila <khursanlyudmila@gmail.com>
2023-02-01 13:34:44 +0300

    add Home Work Task 58

---
+++ HomeWork_Task58/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+int[,] matrix = InitMatrix(3, 3);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int[,] secondMatrix = InitMatrix(3, 3);
+PrintMatrix(secondMatrix);
+Console.WriteLine();
+
+int[,] resultMatrix = InitMatrix(3, 3);
+Console.WriteLine();
+
+if (matrix.GetLength(0) != secondMatrix.GetLength(1))
+{
+    Console.WriteLine("Матрицы нельзя перемножить!");
+    return;
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondMatrix.GetLength(1); j++)
+    {
+        resultMatrix[i, j] = 0;
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            resultMatrix[i, j] += matrix[i, k] * secondMatrix[k, j];
+        }
+    }
+}
+PrintMatrix(resultMatrix);
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
