2d7b7faa1122542b4762e735cf26e4e4adc80ef6
Khursan Lyudmila <khursanlyudmila@gmail.com>
2023-02-01 13:34:20 +0300

    add Home Work Task 54

---
+++ HomeWork_Task54/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void orderDescending(int[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, j] <= matrix[i, k])
+                    continue;
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[i, k];
+                matrix[i, k] = temp;
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = InitMatrix(5, 5);
+Console.WriteLine("Изначальная матрица");
+PrintMatrix(matrix);
+
+orderDescending (matrix);
+Console.WriteLine("Преобразованная по убыванию по каждой строке матрица");
+PrintMatrix(matrix);
+
